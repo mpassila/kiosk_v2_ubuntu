10e63f52157c612ef4f483afa74667add6940e51
Mika Kristian Passila <mika.passila@gmail.com>
2026-03-06 12:08:23 +0200

    Release build - Fri 03/06/2026 12:08:23.26

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 KIOSK_DIR="$SCRIPT_DIR/kiosk"
 
-sudo xinit /usr/bin/electron "$KIOSK_DIR/release/app/dist/main/main.js" --no-sandbox --remote-debugging-port=9222 --devtools -- :0 -nocursor
+sudo xinit /usr/bin/electron "$KIOSK_DIR/release/app/dist/main/main.js" --no-sandbox --remote-debugging-port=9222 --devtools -- :0
